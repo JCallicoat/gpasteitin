76654dab70ce2e93fa78ec10dc13c3a7877aac79
MonkeeSage <MonkeeSage@localhost>
2010-09-02 02:36:07 -0500

    Initial Commit

---
+++ gpasteitin/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import os, glob
+from distutils.core import setup
+
+dfiles     = (["resources/gpasteitin.ui"])
+scripts    = [os.path.join("scripts", "gpasteitin")]
+data_files = [("/usr/share/gpasteitin", dfiles),
+              ("/usr/share/pixmaps", ["resources/gpasteitin.png"]),
+              ("/usr/share/applications", ["resources/gpasteitin.desktop"])]
+
+setup(name             = "GPasteItIn",
+      version          = "0.7",
+      description      = "Simple snippet / paste tool for GTK+",
+      long_description = "Simple snippet / paste tool for GTK+",
+      author           = "Jordan Callicoat",
+      author_email     = "jordan.callicoat@rackspace.com",
+      url              = "",
+      download_url     = "",
+      license          = "BSD",
+      platforms        = ["POSIX", "OS X", "Windows"],
+      keywords         = [],
+      packages         = ["gpasteitin"],
+      scripts          = scripts,
+      data_files       = data_files,
+      )
