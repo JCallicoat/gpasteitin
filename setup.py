42ec1af1b169a61234906410d6928a20526e0577
MonkeeSage <MonkeeSage@localhost>
2010-10-09 03:35:28 -0500

    Add a 100ms delay between setting clipboard text and sending the paste keypress. Bump to 0.7.4.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 import os, glob
 from distutils.core import setup
-from gpasteitin import __VERSION__
+from gpasteitin import gpasteitin
 
 dfiles     = (["resources/gpasteitin.ui"])
 scripts    = [os.path.join("scripts", "gpasteitin")]
@@ -11,7 +11,7 @@ data_files = [("/usr/share/gpasteitin", dfiles),
               ("/usr/share/applications", ["resources/gpasteitin.desktop"])]
 
 setup(name             = "GPasteItIn",
-      version          = __VERSION__,
+      version          = gpasteitin.__VERSION__,
       description      = "Simple snippet / paste tool for GTK+",
       long_description = "Simple snippet / paste tool for GTK+",
       author           = "Jordan Callicoat",
