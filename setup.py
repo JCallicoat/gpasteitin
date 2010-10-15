3cfd58317ee688b06da1d3aafb9a5023173203cf
MonkeeSage <MonkeeSage@localhost>
2010-10-15 03:38:56 -0500

    Merging

--- setup.py
+++ setup.py
@@ -2,11 +2,7 @@
 
 import os, glob, sys
 from distutils.core import setup
-<<<<<<< local
 import version
-=======
-# from gpasteitin import gpasteitin
->>>>>>> other
 
 dfiles     = (["resources/gpasteitin.ui"])
 scripts    = [os.path.join("scripts", "gpasteitin")]
@@ -15,12 +11,7 @@ data_files = [("/usr/share/gpasteitin", dfiles),
               ("/usr/share/applications", ["resources/gpasteitin.desktop"])]
 
 setup(name             = "GPasteItIn",
-<<<<<<< local
       version          = version.__VERSION__,
-=======
-#       version          = gpasteitin.__VERSION__,
-      version          = "0.7.4",
->>>>>>> other
       description      = "Simple snippet / paste tool for GTK+",
       long_description = "Simple snippet / paste tool for GTK+",
       author           = "Jordan Callicoat",
